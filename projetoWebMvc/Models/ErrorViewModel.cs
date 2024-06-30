56e507d030491c7696bf33569c0e33f8f1ee85ec
Rafael <rafael.ps0007@gmail.com>
2024-06-30 18:41:00 -0300

    primeiro commit

---
+++ projetoWebMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace projetoWebMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
